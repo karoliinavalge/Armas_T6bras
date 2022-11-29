c9136368bc9148abf110353fc1e923ba8b2f7512
Vanessa Apuhtin <vanessaapuhtin@Vanessas-MBP-2.lan>
2022-11-29 21:52:17 +0200

    enamvähem valmis avaleht

--- avaleht.js
+++ avaleht.js
@@ -1,15 +1,28 @@
-const cursorRounded = document.querySelector('.rounded');
-const cursorPointed = document.querySelector('.pointed');
+/* võetud siit lehelt https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_slideshow */
 
-
-const moveCursor = (e)=> {
-  const mouseY = e.clientY;
-  const mouseX = e.clientX;
-   
-  cursorRounded.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
-  
-  cursorPointed.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
- 
-}
-
-window.addEventListener('mousemove', moveCursor)
+let slideIndex = 1;
+        showSlides(slideIndex);
+        
+        function plusSlides(n) {
+          showSlides(slideIndex += n);
+        }
+        
+        function currentSlide(n) {
+          showSlides(slideIndex = n);
+        }
+        
+        function showSlides(n) {
+          let i;
+          let slides = document.getElementsByClassName("mySlides");
+          let dots = document.getElementsByClassName("dot");
+          if (n > slides.length) {slideIndex = 1}    
+          if (n < 1) {slideIndex = slides.length}
+          for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";  
+          }
+          for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+          }
+          slides[slideIndex-1].style.display = "block";  
+          dots[slideIndex-1].className += " active";
+        }
